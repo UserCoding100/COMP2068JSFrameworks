f7747ad350409182a7a66d16712e718c7a4e0dc1
UserCoding100 <boombeach3443@gmail.com>
2024-12-05 21:43:14 -0500

    Deletion of two files
    Deleted some unnecessary files that wasn't really contributing to the needs of the assignment.

--- ASSIGNMENT02/routes/public-data.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('public-data', { title: 'Public Data' });
-});
-
-module.exports = router; // Ensures this exports the `router`
